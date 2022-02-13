ebb0697a706992d7df2fa98bf920c983960ed820
MadamGervas <darya.gervas@mail.ru>
2022-02-13 17:17:01 +0700

    add toggle

--- script/script.js
+++ script/script.js
@@ -2,7 +2,5 @@ let page = document.querySelector('.page');
 
 let themeButton = document.querySelector('.theme-button');
 themeButton.onclick = function () {
-    console.log('Кнопка нажата!');
-    page.classList.remove('light-theme');
-    page.classList.add('dark-theme');
+    page.classList.toggle('dark-theme');
 };
